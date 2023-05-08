b4b6da1206bfa7446717175b04198db6cdf37417
Aram Zucker-Scharff <azuckers@gmu.edu>
2023-05-08 11:58:54 -0400

    Update lfgtopics.js

--- docs/lfgtopics.js
+++ docs/lfgtopics.js
@@ -1,2 +1,6 @@
 // document.browsingTopics() returns an array of up to three topic objects in random order.
 const topics = await document.browsingTopics();
+
+// Create an iframe
+// Create a post-message mechanism 
+// send topics via postMessage
