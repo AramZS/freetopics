cc916792f78b14967a2ec5a82d653a3a316f4e28
Aram Zucker-Scharff <azuckers@gmu.edu>
2023-05-08 16:24:14 -0400

    Update lfgtopics.js

--- docs/lfgtopics.js
+++ docs/lfgtopics.js
@@ -22,6 +22,8 @@ if (!document.getElementById('lfgtopics')){
   let iFrame = document.createElement('iframe');
   iFrame.id = "lfgtopics";
   iFrame.src = `https://free.topics.center/iframe-contents.html?cb=12312323sadf`;
+  iFrame.width = 0;
+  iFrame.height = 0;
   document.body.prepend(iFrame);
 }
 
