8c2ade4156af2697047aba8d71ad1c5929530a1b
Aram Zucker-Scharff <azuckers@gmu.edu>
2023-05-08 13:06:39 -0400

    Update lfgtopics.js
    only call once

--- docs/lfgtopics.js
+++ docs/lfgtopics.js
@@ -1,10 +1,15 @@
 (async function(){
-// https://github.com/patcg-individual-drafts/topics
-// document.browsingTopics() returns an array of up to three topic objects in random order.
-const topics = await document.browsingTopics();
-console.log("Let's Fking Go get your topics", topics);
+  window.lfgtopics = window.lfgtopics ? window.lfgtopics : false;
+  if (window.lfgtopics === false){
+    // https://github.com/patcg-individual-drafts/topics
+    // document.browsingTopics() returns an array of up to three topic objects in random order.
+    const topics = await document.browsingTopics();
+    console.log("Let's Fking Go get your topics", topics);
 
-// Create an iframe
-// Create a post-message mechanism 
-// send topics via postMessage
+
+    // Create an iframe
+    // Create a post-message mechanism 
+    // send topics via postMessage
+    window.lfgtopics = true;
+  }
 })()
