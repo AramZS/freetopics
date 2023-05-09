6017a9322572eb1e6fdf995ba465344ea5f1bb73
Aram Zucker-Scharff <azuckers@gmu.edu>
2023-05-09 10:26:42 -0400

    Update lfgtopics.js

--- docs/lfgtopics.js
+++ docs/lfgtopics.js
@@ -1,25 +1,3 @@
-let setup = `<script>(async function(){
-  window.lfgtopics = window.lfgtopics ? window.lfgtopics : false;
-  if (window.lfgtopics === false){
-    window.lfgtopics = true;
-    // https://github.com/patcg-individual-drafts/topics
-    // https://github.com/dgirardi/Prebid.js/blob/c0a922cc59f074aa98a2784fe1d4e794c5da0a46/modules/topicsFpdModule.js
-    // https://developer.chrome.com/docs/privacy-sandbox/topics/overview/
-    // Turn on Privacy Sandbox Ads API here: chrome://flags/
-    // See your Topics here: chrome://topics-internals/
-    // Get some cool topics - https://trackthis.link/
-    // https://topics-demo.glitch.me/
-    // /Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary --enable-features=BrowsingTopics:time_period_per_epoch/15s,PrivacySandboxAdsAPIsOverride,PrivacySandboxSettings3,OverridePrivacySandboxSettingsLocalTesting
-    // document.browsingTopics() returns an array of up to three topic objects in random order.
-    const topics = await document.browsingTopics();
-    console.log("Let's Fking Go get your topics v0.0.4", topics);
-
-
-    // Create an iframe
-    // Create a post-message mechanism 
-    // send topics via postMessage
-  }
-})()</script>`;
 if (!document.getElementById('lfgtopics')){
   let iFrame = document.createElement('iframe');
   iFrame.id = "lfgtopics";
